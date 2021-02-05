a9f15ac7bb3e3eb157abfaedc0c609cae40744fb
Nick <nick.reynolds@discordapp.com>
2021-02-04 16:44:38 -0800

    magic fortune cookie emoji

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to your 🥠
+          Welcome to your <br /> 🧙🔮🍪
         </h1>
 
         <p className={styles.description}>
